a1741d13fce534f1c6545bce3562306a5dfde4b5
°D <dcqout@gmail.com>
2026-02-06 19:29:50 -0300

    Sync

--- fabric-1.21.11/src/main/java/dcq/wlog/BlockIn.java
+++
@@ -1,8 +0,0 @@
-package dcq.wlog;
-
-import net.minecraft.world.level.block.state.BlockState;
-
-public interface BlockIn {
-    BlockState getBlockState();
-    void regDefaultState(BlockState blockState);
-}
